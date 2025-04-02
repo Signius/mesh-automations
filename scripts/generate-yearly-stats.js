3cc749af597425f854d74f55ac3658d3a3d16ad8
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2025-04-02 08:54:07 +0200

    Update March statistics in mesh documentation and adjust markdown table formatting for improved alignment in GitHub usage statistics.

--- scripts/generate-yearly-stats.js
+++ scripts/generate-yearly-stats.js
@@ -130,7 +130,7 @@ ${monthlyDownloads.core.map(m => {
 
 ## 🔍 GitHub Usage Statistics
 
-| Month${'&nbsp;'.repeat(44)} |   Project Count |   File Count |
+| Month${'&nbsp;'.repeat(44)} |   Projects |   Files |
 | :---------------------------------------- | -------------: | -----------: |
 ${monthNames.map(month => {
         const monthStats = githubStats[month] || { core_in_package_json: 0, core_in_any_file: 0 };
