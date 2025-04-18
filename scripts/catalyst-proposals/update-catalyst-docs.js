f9bc4bfe7f39dc5822b5253eea85ea7b40d49463
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2025-04-18 12:39:04 +0200

    Set buildId to a hardcoded value in update-catalyst-docs.js for testing purposes

--- scripts/catalyst-proposals/update-catalyst-docs.js
+++ scripts/catalyst-proposals/update-catalyst-docs.js
@@ -4,7 +4,7 @@ import { saveCatalystData } from './save-catalyst-data.js';
 import { fetchProposalFromChallenge } from './fetch-proposal-from-challenge.js';
 
 // Environment checks
-const buildId      = process.env.NEXT_PUBLIC_BUILD_ID;
+const buildId      = 'pJZYf0Bzp4nPDQmwjxLiJ';
 const supabaseUrl  = process.env.NEXT_PUBLIC_SUPABASE_URL2;
 const supabaseKey  = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY2;
 const USE_MOCK_DATA = !supabaseUrl || !supabaseKey;
